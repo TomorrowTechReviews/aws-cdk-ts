437e7877c4136658bead057f2ca672eafe3d94f1
Sergey Onufrienko <sergey@onufrienko.com>
2024-04-04 09:30:39 +0100

    feat: add load_cpu with fibonacci

--- containers/chats/app/routers/chats.py
+++ containers/chats/app/routers/chats.py
@@ -9,11 +9,29 @@ from .. import auth
 router = APIRouter()
 
 
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def load_cpu():
+    results = []
+    for n in range(10):
+        result = fibonacci(12)
+        results.append(result)
+    return results
+
+
 @router.get("/chats", response_model=list[Chat])
 def list_chats(
     user: Annotated[AuthUser, Depends(auth.get_current_user)],
     db: Session = Depends(get_db),
 ):
+    load_cpu()
     return crud.get_chats_by_user(db=db, user_id=user.id)
 
 
